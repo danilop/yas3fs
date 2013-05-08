25ec6b9409c00ac3e9833440e45e435372c729bd
Danilo Poccia <dpoccia@gmail.com>
2013-05-08 11:57:48 +0200

    Stress test too added

--- Test/parallel.sh
+++ Test/parallel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 NUMBER=$1
-CMD=$2
+CMD="$2 $3 $4 $5 $6 $7 $8 $9"
 
 for i in `seq 1 1 $NUMBER`
 do
--- Test/test-yas3fs.sh
+++ Test/test-yas3fs.sh
@@ -14,7 +14,7 @@ do
     FILENAME=`echo "$FILELIST"|sed -n ${N}p`
     echo "File name is: $FILENAME"
 
-    SIZE=`cat $FILENAME|wc -c`
+    SIZE=`cat "$FILENAME"|wc -c`
     echo "File size is: $SIZE"
 
     START=`echo "$RANDOM * $SIZE / 32767"|bc`
@@ -23,8 +23,8 @@ do
     END=`echo "$START + $RANDOM * ($SIZE - $START) / 32767"|bc`
     echo "End is: $END" 
 
-    echo "Reading "$FILENAME" form $START to $END ..."
-    cut -b $START-$END $FILENAME > /dev/null
+    echo "Reading $FILENAME form $START to $END ..."
+    cut -b $START-$END "$FILENAME" > /dev/null
     echo "Done!"
 
 done
